9ff90c3d93c67171a7fcfa0ca89239cf559088a2
Stephen Haywood <stephen@averagesecurityguy.info>
2018-02-27 23:16:38 -0500

    Reorganize

---
+++ set/intset.go
@@ -0,0 +1,122 @@
+// The set package implements a set of integers.
+package set
+
+import (
+	"sort"
+)
+
+// Set defines a new set of integers.
+type IntSet struct {
+	members map[int]struct{}
+}
+
+// Add places a new member in the set.
+func (s *IntSet) Add(i int) {
+	_, ok := s.members[i]
+	if !ok {
+		s.members[i] = struct{}{}
+	}
+}
+
+// Remove takes a member out of the set.
+func (s *IntSet) Remove(i int) {
+	delete(s.members, i)
+}
+
+// Contains returns true if i is a member of the set.
+func (s *IntSet) Contains(i int) bool {
+	_, ok := s.members[i]
+
+	return ok
+}
+
+// Members returns a list of the members of the set.
+func (s *IntSet) Members() []int {
+	var members []int
+
+	for i := range s.members {
+		members = append(members, i)
+	}
+
+	sort.Ints(members)
+
+	return members
+}
+
+// Size returns the number of members in the set.
+func (s *IntSet) Size() int {
+	return len(s.members)
+}
+
+// Equal returns true if the given set is the same length and contains all of the
+// same members as this set.
+func (s *IntSet) Equal(y *IntSet) bool {
+	if s.Size() != y.Size() {
+		return false
+	}
+
+	for _, m := range s.Members() {
+		if !y.Contains(m) {
+			return false
+		}
+	}
+
+	return true
+}
+
+// Union returns a new set that contains all of the members of this set and
+// all of the members of the given set y.
+func (s *IntSet) Union(y *IntSet) *IntSet {
+	u := NewIntSet([]int{})
+
+	for _, m := range s.Members() {
+		u.Add(m)
+	}
+
+	for _, m := range y.Members() {
+		u.Add(m)
+	}
+
+	return u
+}
+
+// Intersection returns a new set that contains only those members that appear
+// in both this set and the given set y.
+func (s *IntSet) Intersection(y *IntSet) *IntSet {
+	i := NewIntSet([]int{})
+
+	for _, m := range s.Members() {
+		if y.Contains(m) {
+			i.Add(m)
+		}
+	}
+
+	return i
+}
+
+// Difference returns a new set that contains only the members of this set
+// that are not also members of the given set y.
+func (s *IntSet) Difference(y *IntSet) *IntSet {
+	d := NewIntSet([]int{})
+
+	for _, m := range s.Members() {
+		if !y.Contains(m) {
+			d.Add(m)
+		}
+	}
+
+	return d
+}
+
+// NewIntSet returns a new set of integers containing the unique integers in
+// the members slice.
+func NewIntSet(members []int) *IntSet {
+	i := new(IntSet)
+	i.members = make(map[int]struct{})
+
+	for _, m := range members {
+		i.Add(m)
+	}
+
+	return i
+}
--- set/stringset_test.go
+++ set/stringset_test.go
@@ -1,63 +1,3 @@
-package set
-
-import (
-	"testing"
-)
-
-func TestIntSet(t *testing.T) {
-	s := NewIntSet([]int{})
-
-	s.Add(1)
-	if !s.Contains(1) {
-		t.Error("1 is not a member of the set.")
-	}
-
-	s.Add(2)
-	if !s.Contains(2) {
-		t.Error("2 is not a member of the set.")
-	}
-
-	if s.Size() != 2 {
-		t.Error("Set should contain two values.")
-	}
-
-	s.Remove(0)
-	if s.Size() != 2 {
-		t.Error("Set should contain two values.")
-	}
-
-	s.Remove(1)
-	if s.Size() != 1 {
-		t.Error("Set should contain one value.")
-	}
-
-	allSlice := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
-	all := NewIntSet(allSlice)
-	members := all.Members()
-
-	if !equalIntSlice(members, allSlice) {
-		t.Error("Set of all values does not contain the expected members.")
-	}
-
-	even := NewIntSet([]int{0, 2, 4, 6, 8})
-	odd := NewIntSet([]int{1, 3, 5, 7, 9})
-	intersect := all.Intersection(even)
-	union := all.Union(odd)
-	diff := all.Difference(even)
-
-	if !intersect.Equal(even) {
-		t.Error("The intersection of all and even should be even.")
-	}
-
-	if !union.Equal(all) {
-		t.Error("The union of all and odd should be all.")
-	}
-
-	if !diff.Equal(odd) {
-		t.Error("The difference of all and even should be odd.")
-	}
-}
-
 func TestStringSet(t *testing.T) {
 	s := NewStringSet([]string{})
 
@@ -113,20 +53,6 @@ func TestStringSet(t *testing.T) {
 }
 
 func equalStringSlice(a, b []string) bool {
-	if len(a) != len(b) {
-		return false
-	}
-
-	for i := range a {
-		if a[i] != b[i] {
-			return false
-		}
-	}
-
-	return true
-}
-
-func equalIntSlice(a, b []int) bool {
 	if len(a) != len(b) {
 		return false
 	}
